5f49454063b755f1e564ee4e8c5576fe12c36fd2
ashrafur22 <ashrafurrahman22@gmail.com>
2022-05-23 23:47:16 +0600

    initial-setup-complete

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.buvu7.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -23,6 +23,15 @@ async function run(){
             const products = await cursor.toArray();
             res.send(products);
         })
+
+        // catch single item
+        app.get('/product/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        })
+
     }
     finally {
 
