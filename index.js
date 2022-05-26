5ec07f8d8e16dc335dd9f8ea445ed1ceef673cf5
ashrafur22 <ashrafurrahman22@gmail.com>
2022-05-26 12:32:13 +0600

    single-api-updated

--- index.js
+++ index.js
@@ -50,6 +50,7 @@ async function run(){
             res.send(result);
         });
 
+        // catch single item
         app.get('/purchase/:id', async(req, res)=>{
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
